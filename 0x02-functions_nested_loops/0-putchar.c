5ebea15ad0e3064c36fb81fe1f6fa73cca535792
BrianNjugush <njugunabrian95@gmail.com>
2022-07-07 10:44:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Check description
- * Description: It prints the word Holberton, followed by a new line
+ * Description: It prints the word Holberton, followed by a new line.
  * Return: 0.
  */
 int main(void)
@@ -17,4 +17,3 @@ int main(void)
 
 	return (0);
 }
-
